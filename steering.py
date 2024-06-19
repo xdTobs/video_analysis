33f894d016f28d14d89ce323d2f3c71555d6b70d
Loui Nissen-Petersen <s194294@dtu.dk>
2024-06-19 12:37:50 +0200

    move corrected

--- steering.py
+++ steering.py
@@ -224,25 +224,23 @@ class Steering:
             print(f"Error: {e}")
             return
 
-    def get_near_ball(self, signed_angle_degrees, angle_degrees, dist_to_ball):
-        if -8 < angle_degrees < 8:
-            self.robot_interface.send_command("move", 100, 60)
-        else:
+
+    def move_corrected(self, signed_angle_degrees, angle_degrees, speed):
+        print(f"angle to target {angle_degrees}")
+        if angle_degrees < 2:
+            self.robot_interface.send_command("move", 100, speed)
+        elif 2 <= angle_degrees <= 8:
+            self.robot_interface.send_command("move-corrected", -1 * signed_angle_degrees, speed)
+            print(f"Signed angle degrees {signed_angle_degrees}")
+        elif angle_degrees > 8:
             turn = signed_angle_degrees * -1 / 3
             self.robot_interface.send_command("turn", turn, 30)
-        # if -8 < angle_degrees < 8:
-        #     self.robot_interface.send_command("move", 100, 20)
-        #     print("Moving forward")
-        # else:
-        #     turn = signed_angle_degrees * -1 / 3
-        #     self.robot_interface.send_command("turn", turn, 20)
+
+    def get_near_ball(self, signed_angle_degrees, angle_degrees, dist_to_ball):
+        self.move_corrected(signed_angle_degrees, angle_degrees, 100)
 
     def collect_ball(self, signed_angle_degrees, angle_degrees, dist_to_ball):
-        if -3 < angle_degrees < 3:
-            self.robot_interface.send_command("move", 100, 15)
-        else:
-            turn = signed_angle_degrees * -1 / 3
-            self.robot_interface.send_command("turn", turn, 20)
+        self.move_corrected(signed_angle_degrees, angle_degrees, 100)
 
     def start_belt(self):
         self.robot_interface.send_command("belt", 0, speedPercentage=100)
