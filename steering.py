79aeb70b82f5f381c95620668adbed775deb2066
Rolle <rolle474@gmail.com>
2024-06-21 13:58:34 +0200

    revert to original

--- steering.py
+++ steering.py
@@ -41,7 +41,6 @@ class Steering:
         self.target_safepoint_index = None
         self.is_targeting_ball = False
         self.is_targeting_safepoint = False
-        self.should_drive_backwards = False
 
     # checks if we can go to ball without crashing into the mid cross
     def check_no_obstacles(
@@ -100,7 +99,6 @@ class Steering:
             self.target_ball = self.find_closest_ball(keypoints, robot_pos)
         self.path = self.create_path(self.target_ball, robot_pos, safepoint_list)
         if self.are_coordinates_close(self.path[0]) and len(self.path) > 1:
-            self.move_corrected(0, 0, 30)
             self.path.pop(0)
         elif self.can_target_ball_directly(robot_pos, self.target_ball):
             while len(self.path) > 1:
@@ -268,31 +266,22 @@ class Steering:
     def move_corrected(self, signed_angle_degrees, angle_degrees, speed):
         print(f"angle to target {angle_degrees}")
 
-        if self.are_coordinates_close(self.steering_vector):
-            self.should_drive_backwards = False
-        else:
-            self.should_drive_backwards = True
-
-        if self.should_drive_backwards:
-
-            if angle_degrees > 90:
-
-                if signed_angle_degrees < 0:
-                    reverse_signed_angle_degrees = signed_angle_degrees + 180
-                else:
-                    reverse_signed_angle_degrees = signed_angle_degrees - 180
-                self.robot_interface.send_command("move-corrected", reverse_signed_angle_degrees, -30)
-                print(f"Signed angle degrees: {signed_angle_degrees}")
-        else:
-
-            if angle_degrees < 1.5:
-                self.robot_interface.send_command("move", 100, speed)
-            elif 1.5 <= angle_degrees <= 20:
-                self.robot_interface.send_command("move-corrected", -1 * signed_angle_degrees, 40)
-                print(f"Signed angle degrees {signed_angle_degrees}")
+        if angle_degrees > 90:
+            if signed_angle_degrees < 0:
+                reverse_signed_angle_degrees = signed_angle_degrees + 180
             else:
-                turn = signed_angle_degrees * -1 / 3
-                self.robot_interface.send_command("turn", turn, 30)
+                reverse_signed_angle_degrees = signed_angle_degrees - 180
+            self.robot_interface.send_command("move-corrected", reverse_signed_angle_degrees, -30)
+            print(f"Signed angle degrees: {signed_angle_degrees}")
+
+        elif angle_degrees < 1.5:
+            self.robot_interface.send_command("move", 100, speed)
+        elif 1.5 <= angle_degrees <= 20:
+            self.robot_interface.send_command("move-corrected", -1 * signed_angle_degrees, 40)
+            print(f"Signed angle degrees {signed_angle_degrees}")
+        else:
+            turn = signed_angle_degrees * -1 / 3
+            self.robot_interface.send_command("turn", turn, 30)
 
     def get_near_ball(self, signed_angle_degrees, angle_degrees, dist_to_ball):
         self.move_corrected(signed_angle_degrees, angle_degrees, 30)
